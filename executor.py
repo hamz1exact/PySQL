fd52c5ba47581fe9df0f75956550d6ab7cfc0a2a
SU HAMZA <official.hamzaderaoui@gmail.com>
2025-09-24 19:17:18 +0100

    fix bypass checking Null while inserting

--- executor.py
+++ executor.py
@@ -483,8 +483,8 @@ def execute_insert_query(ast, database):
                         raise ValueError(f"Column <{col_object}> cannot be null; it uses a NOT NULL constraint.")
                 else:
                     new_row[col_object] = None
-        
             if col_object in table_restrictions:
+                
                 if not table_restrictions[col_object].evaluate(new_row, table_schema):
                     raise ValueError(f"new row for relation '{table_name}' violates check constraint of column <{col_object}>")
         
